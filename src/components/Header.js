7802a55616aae1b4087c32e407558f6f95a3b93a
Jupiter <jupiterbaudot@gmail.com>
2018-03-21 01:10:05 -0700

    Refactor Header with implicit return

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//we implicitly return stateless components. So using return is not necessary
+// remove return and replace opening => { with => (
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      {props.subtitle && <h2>{props.subtitle}</h2>}
-    </div>
-  );
-};
+const Header = (props) => (
+  <div>
+    <h1>{props.title}</h1>
+    {props.subtitle && <h2>{props.subtitle}</h2>}
+  </div>
+)
+
+
+
+
 
 Header.defaultProps = {
   title: 'Indecision'
