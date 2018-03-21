6f7b4d168eeb82e1aba96ec31de6f5be899b3941
Jupiter <jupiterbaudot@gmail.com>
2018-03-21 16:16:03 -0700

    Further styling

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -126,23 +126,24 @@ class IndecisionApp extends React.Component {
 
     return (
       <div className="">
+
         <Header subtitle={subtitle} />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
 
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-
-        <OptionModal selectedOption={this.state.selectedOption} handleClearSelectedOption={this.handleClearSelectedOption}/>
-
+          <OptionModal selectedOption={this.state.selectedOption} handleClearSelectedOption={this.handleClearSelectedOption}/>
+        </div>
       </div>
 
     );
